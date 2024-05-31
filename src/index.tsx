0f305cafbcfe1581b5fb98baf7e0555e0cc8e431
Yy <akumakumu@gmail.com>
2024-06-01 00:30:05 +0700

    adding elysia-tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import { Elysia } from "elysia";
 import { html } from '@elysiajs/html';
+import { tailwind } from "@gtramontina.com/elysia-tailwind";
 
 const app = new Elysia()
   .use(html())
+  .use(tailwind({
+    path: "/public/stylesheet.css",
+    source: "./source/styles.css",
+    config: "./tailwind.config.js",
+    options: {
+        minify: true,
+        map: true,
+        autoprefixer: false
+    },
+  }))
   .get("/", () => (
     <html lang="en">
       
